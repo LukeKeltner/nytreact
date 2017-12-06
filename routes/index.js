34384546d6227626fd46747cfa73ac072254735a
Luke Keltner <lkeltner87@gmail.com>
2017-12-06 05:50:40 -0500

    getting home

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,10 @@ router.route("/hi")
 router.route("/hi/:title")
 	.delete(articleController.remove)
 
-/*// If no API routes are hit, send the React app
+// If no API routes are hit, send the React app
 router.use(function(req, res) 
 {
   	res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
-*/
+
 module.exports = router;
